7747bc285fec1490d1865b4235aeef8c483da620
Bhebhe <linda.bhebhe@ashesi.edu.gh>
2017-01-20 17:15:34 +0100

    Have implemented the addition

---
+++ Calc.java
@@ -0,0 +1,15 @@
+import java.util.Scanner
+public class SimpleCalculator{
+  
+ float num1 =0, num2 = 0;
+
+  public static void main(String args []){
+Scanner input = S
+    System.out.println("Write two numbers and + to add them");
+    
+num1 = input.nextFloat();
+num2 = input.nextFloat();
+yes
+
+  }
+}
